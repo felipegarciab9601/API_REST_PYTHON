0f020d0c63966bda7d1a88f4468dcb54ef5a72e1
Felipe Garcia <felipegarciab9631@hotmail.com>
2020-04-27 18:30:18 -0500

    Implementation of the method GET to the API

--- src/app.py
+++ src/app.py
@@ -32,9 +32,52 @@ tasks_schema = TaskSchema(many=True)
 
 @app.route('/tasks', methods=['Post'])
 def create_task():
-    print(request.json)
-    return 'received'
+    title = request.json['title']
+    description = request.json['description']
 
+    new_task= Task(title, description)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return task_schema.jsonify(new_task)
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  all_tasks = Task.query.all()
+  result = tasks_schema.dump(all_tasks)
+  return jsonify(result)
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+  task = Task.query.get(id)
+  return task_schema.jsonify(task)
+
+@app.route('/tasks/<id>', methods=['PUT'])
+def update_task(id):
+  task = Task.query.get(id)
+
+  title = request.json['title']
+  description = request.json['description']
+
+  task.title = title
+  task.description = description
+
+  db.session.commit()
+
+  return task_schema.jsonify(task)
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+  task = Task.query.get(id)
+  db.session.delete(task)
+  db.session.commit()
+  return task_schema.jsonify(task)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to my API'})
 
 if __name__ == "__main__":
     app.run(debug=True)
